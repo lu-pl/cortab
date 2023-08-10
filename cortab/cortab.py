db65beff6e5b98bb1a1dc2b1e9867bbbc27e088f
Lukas Plank <lupl@tuta.io>
2023-08-10 09:14:18 +0200

    Cleared conflict

--- cortab/cortab.py
+++ cortab/cortab.py
@@ -4,16 +4,8 @@ from clisn import CLSInfraNamespaceManager
 from tabulardf import FieldGraphConverter
 from rdflib import Graph
 
-<<<<<<< HEAD:cortab/dfconvert.py
-from rules import rules
-# from clsns import CLSInfraNamespaceManager
-from clisn import CLSInfraNamespaceManager
-
-from table_partitions import (
-=======
 from cortab.rules import rules
 from cortab.table_partitions import (
->>>>>>> port:cortab/cortab.py
     corpus_table,
     rem_partition,
     greekdracor_partition,
